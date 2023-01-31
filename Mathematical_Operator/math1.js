51e340717b822eeeb30827411ca5945be68cffa5
Badri vishal singh <badri.singh8090@gmail.com>
2023-01-31 17:29:09 +0530

    Mathmetical_operators_files

---
+++ Mathematical_Operator/math1.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 2;
+console.log("sum is",a+b);
+console.log("sub is",a-b);
+console.log("multiplication is",a*b);
+console.log("division is",a/b);
+console.log(2**3);
+console.log(16**(1/2));
+console.log(typeof(1+"goku"))
---
+++ Mathematical_Operator/math4.js
@@ -0,0 +1,7 @@
+let one=5;
+let two=6;
+let three=7;
+let four=8;
+let five=9;
+let six=1;
+console.log(one*two*three*four*five*six)
