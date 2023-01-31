94201e5cf8c1331ff26c0b42bf9be4dd2e761300
Badri vishal singh <badri.singh8090@gmail.com>
2023-01-31 16:31:22 +0530

    Js_variable_prob_files

---
+++ Introduction_to_Javascript/pro2.js
@@ -0,0 +1,11 @@
+let name="Badri Vishal Singh";
+
+console.log(name);
+
+name= "Fateh Bahadur Singh";
+
+console.log(name);
+
+name="Saroj Singh";
+
+console.log(name);
---
+++ Introduction_to_Javascript/pro3.js
@@ -0,0 +1,4 @@
+let name="Badri Vishal Singh";
+let age= 24;
+console.log(name,age);
+console.log(typeof(name),typeof(age));
